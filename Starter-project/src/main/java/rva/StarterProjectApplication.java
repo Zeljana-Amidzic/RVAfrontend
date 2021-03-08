f7ff96f25ed8220773b64bb2117e25539a07702b
Zeljana <Zeljana@DESKTOP-9A0684F>
2021-03-08 11:48:00 +0100

    vezbe2

--- Starter-project/src/main/java/rva/StarterProjectApplication.java
+++ Starter-project/src/main/java/rva/StarterProjectApplication.java
@@ -1,7 +1,11 @@
 package rva;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 // GitHub Classroom Invite Link --> https://classroom.github.com/a/PgzNRc2E
 
@@ -10,6 +14,20 @@ public class StarterProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StarterProjectApplication.class, args); 
+	}
+	
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		
+		return args -> {
+			System.out.println("Beans provided by spring Boot: ");
+			String[] beans = ctx.getBeanDefinitionNames();
+			
+			Arrays.sort(beans);
+			for(String beansName : beans) {
+				System.out.println(beansName);
+			}
+		};
+		
 	}
 
 }
