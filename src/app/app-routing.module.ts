4182437be57f7098c900e947273262500d09619d
Zeljana-Amidzic <zeljana.amidzic@uns.ac.rs>
2021-06-02 16:57:27 +0200

    za test prvi kommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StatusComponent } from './components/status/status.component';
+import { FakultetComponent } from './components/fakultet/fakultet.component';
+import { DepartmanComponent } from './components/departman/departman.component';
+import { HomeComponent } from './components/core/home/home.component';
+import { AboutComponent } from './components/core/about/about.component';
+import { AuthorComponent } from './components/core/author/author.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'status', component: StatusComponent},
+  {path: 'fakultet', component: FakultetComponent},
+  {path: 'departman', component: DepartmanComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'author', component: AuthorComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { HomeComponent } from './components/core/home/home.component';
+import { AuthorComponent } from './components/core/author/author.component';
+import { AboutComponent } from './components/core/about/about.component';
+import { FakultetComponent } from './components/fakultet/fakultet.component';
+import { StatusComponent } from './components/status/status.component';
+import { DepartmanComponent } from './components/departman/departman.component';
+import { StudentComponent } from './components/student/student.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AuthorComponent,
+    AboutComponent,
+    FakultetComponent,
+    StatusComponent,
+    DepartmanComponent,
+    StudentComponent
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
