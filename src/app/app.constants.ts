f364522f70ec16331c67511aa2593fc6a6d54a23
Zeljana-Amidzic <zeljana.amidzic@uns.ac.rs>
2021-06-09 13:05:09 +0200

    redirects file

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,12 @@
+/*
 export const Departman_URL = 'http://localhost:8083/departman';
 export const Status_URL = 'http://localhost:8083/status';
 export const Fakultet_URL = 'http://localhost:8083/fakultet';
 export const Student_URL = 'http://localhost:8083/student';
 export const StudentDepartman_URL = 'http://localhost:8083/studentDepartmanID';
+*/
+export const Departman_URL = 'https://rva-backend-zeljana-amidzic.herokuapp.com/departman';
+export const Status_URL = 'https://rva-backend-zeljana-amidzic.herokuapp.com/status';
+export const Fakultet_URL = 'https://rva-backend-zeljana-amidzic.herokuapp.com/fakultet';
+export const Student_URL = 'https://rva-backend-zeljana-amidzic.herokuapp.com/student';
+export const StudentDepartman_URL = 'https://rva-backend-zeljana-amidzic.herokuapp.com/studentDepartmanID';
