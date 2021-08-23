7fb5d2b8bf159cc54eda03a42b125114f7c24c7a
Zeljana-Amidzic <zeljana.amidzic@uns.ac.rs>
2021-08-23 16:49:57 +0200

    posts

--- src/app/services/status.service.ts
+++ src/app/services/status.service.ts
@@ -10,18 +10,21 @@ import { Status_URL } from '../app.constants';
 
 export class StatusService {
 
-    constructor(public httpClient: HttpClient) { }
+    constructor(private httpClient: HttpClient) { }
 
     public getAllStatuse(): Observable<any> {
-    return this.httpClient.get(`${Status_URL}`);
+        return this.httpClient.get(`${Status_URL}`);
     }
+
     public addStatus(status: Status): Observable<any> {
         status.id=0;
-        return this.httpClient.post(`${Status_URL}`,status);
+        return this.httpClient.post(`${Status_URL}`, status);
     }
+
     public updateStatus(status: Status): Observable<any> {
-        return this.httpClient.put(`${Status_URL}`,status);
+        return this.httpClient.put(`${Status_URL}`, status);
     }
+
     public deleteStatus(id: number): Observable<any> {
         return this.httpClient.delete(`${Status_URL}/${id}`);
     } 
