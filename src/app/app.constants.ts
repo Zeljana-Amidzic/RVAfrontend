23b6e11fa75d4193be2ec267c62f9f383751ce1f
Zeljana-Amidzic <zeljana.amidzic@uns.ac.rs>
2021-08-29 23:49:19 +0200

    porukaa

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,9 +1,9 @@
 
-export const Departman_URL = 'http://localhost:8083/departman';
+/*export const Departman_URL = 'http://localhost:8083/departman';
 export const Status_URL = 'http://localhost:8083/status';
 export const Fakultet_URL = 'http://localhost:8083/fakultet';
 export const Student_URL = 'http://localhost:8083/student';
-export const StudentDepartman_URL = 'http://localhost:8083/studentDepartmanID';
+export const StudentDepartman_URL = 'http://localhost:8083/studentDepartmanID';*/
 
 /*export const Departman_URL = 'https://rva-backend-zeljana-amidzic.herokuapp.com/departman';
 export const Status_URL = 'https://rva-backend-zeljana-amidzic.herokuapp.com/status';
@@ -12,8 +12,8 @@ export const Student_URL = 'https://rva-backend-zeljana-amidzic.herokuapp.com/st
 export const StudentDepartman_URL = 'https://rva-backend-zeljana-amidzic.herokuapp.com/studentDepartmanID';*/
 
 //https://backend-2-rva.herokuapp.com/status
-/*export const Departman_URL = 'https://backend-2-rva.herokuapp.com/departman';
+export const Departman_URL = 'https://backend-2-rva.herokuapp.com/departman';
 export const Status_URL = 'https://backend-2-rva.herokuapp.com/status';
 export const Fakultet_URL = 'https://backend-2-rva.herokuapp.com/fakultet';
 export const Student_URL = 'https://backend-2-rva.herokuapp.com/student';
-export const StudentDepartman_URL = 'https://backend-2-rva.herokuapp.com/studentDepartmanID';*/
+export const StudentDepartman_URL = 'https://backend-2-rva.herokuapp.com/studentDepartmanID';
